1414f8477b0a2c4883a956376f82eea8d7e4a73d
NicolasT <nicolastem@hotmail.fr>
2022-10-18 11:47:42 +0200

    fix: types definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,6 +8,14 @@ import Vue, { VNode, Component, PluginFunction } from 'vue';
 export type SpinnerType = 'default' | 'bubbles' | 'circles' | 'spiral' | 'waveDots';
 export type DirectionType = 'top' | 'bottom';
 
+export interface Slots {
+  spinner: VNode[];
+  'no-result': VNode[];
+  'no-more': VNode[];
+  'error': VNode[];
+  [key: string]: VNode[];
+}
+
 export interface StateChanger {
   loaded(): void;
   complete(): void;
@@ -15,17 +23,15 @@ export interface StateChanger {
   error(): void;
 }
 
-export interface PluginOptions {
+export interface InfiniteOptions {
   props?: {
     spinner?: SpinnerType;
     distance?: number;
     forceUseInfiniteWrapper?: boolean | string;
   };
-
   system?: {
     throttleLimit?: number;
   };
-
   slots?: {
     noResults?: string | Component;
     noMore?: string | Component;
@@ -35,7 +41,27 @@ export interface PluginOptions {
   };
 }
 
-declare class VueInfiniteLoading {
-  static install: PluginFunction<PluginOptions>
-}
+export default class InfiniteLoading extends Vue {
+  // The trigger distance
+  distance: number;
 
+  // The load spinner type
+  spinner: SpinnerType;
+
+  // The scroll direction
+  direction: DirectionType;
+
+  // Whether find the element which has `infinite-wrapper` attribute as the scroll wrapper
+  forceUseInfiniteWrapper: boolean | string;
+
+  // Infinite event handler
+  onInfinite: ($state: StateChanger) => void;
+
+  // The method collection used to change infinite state
+  stateChanger: StateChanger;
+
+  // Slots
+  $slots: Slots;
+
+  static install: PluginFunction<never>;
+}
