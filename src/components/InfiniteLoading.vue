3837fff17f76c956bfe536a08c57e1c8dd0d935f
Nicolas <chypriote@lolpros.gg>
2022-10-18 11:04:25 +0200

    getScrollParent window

--- src/components/InfiniteLoading.vue
+++ src/components/InfiniteLoading.vue
@@ -316,7 +316,11 @@ export default {
       let result;
 
       if (typeof this.forceUseInfiniteWrapper === 'string') {
-        result = document.querySelector(this.forceUseInfiniteWrapper);
+        if (this.forceUseInfiniteWrapper === 'window') {
+          result = window;
+        } else {
+          result = elm.querySelector(this.forceUseInfiniteWrapper);
+        }
       }
 
       if (!result) {
